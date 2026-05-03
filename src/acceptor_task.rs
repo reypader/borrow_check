3ba1486c7daf30979cea22ab3a9320067c45b575
rmpader <reynaldmpader@gmail.com>
2026-05-03 18:15:51 +0800

    Implemented Journal serialization

--- src/acceptor_task.rs
+++ src/acceptor_task.rs
@@ -1,12 +1,15 @@
-use crate::{AccountId, AccountType, Operation};
+use crate::{Account, AccountId, BalanceType, BookId, Currency, Operation};
+use std::array::TryFromSliceError;
 use std::collections::HashMap;
 use std::fmt::{Display, Formatter};
+use std::io::{Cursor, Write};
 use std::mem::replace;
-use std::time::Duration;
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
 use thiserror::Error;
 use tokio::signal;
 use tokio::sync::{mpsc, oneshot};
 use tokio::time::{Instant, sleep};
+use uuid::Uuid;
 
 pub fn spawn(
     buffer_capacity: usize,
@@ -28,37 +31,199 @@ pub fn spawn(
 }
 
 #[derive(Debug)]
-pub struct ValidOperation(Operation);
+pub struct ValidOperation {
+    idempotency_key: Uuid,
+    entries: Vec<PreProcessedEntry>,
+}
+
+#[derive(Debug)]
+struct PreProcessedEntry {
+    target_account_id: AccountId,
+    target_book_id: BookId,
+    amount: i128,
+    ledger_code: [u8; 8],
+    currency: Currency,
+    balance_type: BalanceType,
+}
+
+#[derive(Debug)]
+struct ProcessedOperation {
+    idempotency_key: Uuid,
+    entries: Vec<ProcessedEntry>,
+    operation_id: u64,
+    checksum: u32,
+    prev_hash: [u8; 32],
+}
+
+#[derive(Debug)]
+struct ProcessedEntry {
+    target_book_id: BookId,
+    amount: i128,
+    ledger_code: [u8; 8],
+    target_page: u32,
+    target_line: u16,
+}
+
+impl ProcessedOperation {
+    fn compacted(self) -> Result<Vec<u8>, WriteMismatchError> {
+        let ProcessedOperation {
+            idempotency_key,
+            entries,
+            checksum,
+            prev_hash,
+            operation_id,
+        } = self;
+        let record_length = 80 + (38 * entries.len());
+        let mut result = Vec::with_capacity(record_length);
+        let header = {
+            let now = SystemTime::now()
+                .duration_since(UNIX_EPOCH)
+                .expect("Time went backwards")
+                .as_nanos();
+            let entry_count = entries.len() as u8; //safe, we'll be limiting the length at the entry point.
+            let mut byte_array = [0u8; 80];
+            let mut write_cursor = Cursor::new(&mut byte_array[..]);
+            write_cursor.write_all(&(record_length as u16).to_le_bytes())?; //2
+            write_cursor.write_all(&entry_count.to_le_bytes())?; //1
+            write_cursor.write_all(&operation_id.to_le_bytes())?; //8
+            write_cursor.write_all(&now.to_le_bytes())?; //16
+            write_cursor.write_all(idempotency_key.as_bytes())?; //16
+            write_cursor.write_all(&checksum.to_le_bytes())?; //4
+            write_cursor.write_all(&prev_hash)?; //32
+            write_cursor.write_all(&0u8.to_le_bytes())?; //8
+
+            let is_filled = write_cursor.position() as usize == write_cursor.get_ref().len();
+            if !is_filled {
+                return Err(WriteMismatchError);
+            } else {
+                byte_array
+            }
+        };
+        result.extend(header);
+
+        for e in entries {
+            let write_entry = {
+                let ProcessedEntry {
+                    target_book_id,
+                    amount,
+                    ledger_code,
+                    target_page,
+                    target_line,
+                } = e;
+                let mut byte_array = [0u8; 38];
+                let mut write_cursor = Cursor::new(&mut byte_array[..]);
+                write_cursor.write_all(&target_book_id.to_le_bytes())?; //8
+                write_cursor.write_all(&target_page.to_le_bytes())?; //4
+                write_cursor.write_all(&target_line.to_le_bytes())?; //2
+                write_cursor.write_all(&amount.to_le_bytes())?; //16
+                write_cursor.write_all(&ledger_code)?; //8
+
+                let is_filled = write_cursor.position() as usize == write_cursor.get_ref().len();
+                if !is_filled {
+                    return Err(WriteMismatchError);
+                } else {
+                    byte_array
+                }
+            };
+            result.extend(write_entry);
+        }
+        Ok(result)
+    }
+}
 
 impl ValidOperation {
-    pub fn parse(op: Operation) -> Result<Self, InvalidOperationError> {
-        let entries = &op.entries;
+    pub fn parse(
+        op: Operation,
+        accounts_in_scope: HashMap<AccountId, Account>,
+    ) -> Result<Self, InvalidOperationError> {
         let mut totals = HashMap::new();
-        for entry in entries {
+        let mut preprocessed_entries = Vec::with_capacity(op.entries.len());
+        for entry in op.entries {
             if entry.amount == 0 {
-                return Err(InvalidOperationError);
+                return Err(InvalidOperationError::InvariantViolation);
             }
-            let currency_total = totals.entry(&entry.currency).or_insert(0);
-            *currency_total += match entry.op_type {
-                AccountType::DEBIT => -entry.amount,
-                AccountType::CREDIT => entry.amount,
+
+            if entry.ledger_code.len() > 8 {
+                return Err(InvalidOperationError::InvariantViolation);
+            }
+            let ledger_code = format!("{: >8}", entry.ledger_code)
+                .into_bytes()
+                .as_slice()
+                .try_into()?;
+
+            let account = accounts_in_scope.get(&entry.account);
+
+            match account {
+                Some(account) => {
+                    let entry_amount = if account.account_type != entry.op_type {
+                        -entry.amount
+                    } else {
+                        entry.amount
+                    };
+                    let target_book = account.books.get(&(entry.currency, entry.balance_type));
+
+                    match target_book {
+                        Some(target_book_id) => {
+                            preprocessed_entries.push(PreProcessedEntry {
+                                target_account_id: entry.account,
+                                target_book_id: *target_book_id,
+                                amount: entry_amount,
+                                ledger_code,
+                                currency: entry.currency,
+                                balance_type: entry.balance_type,
+                            });
+                        }
+                        None => return Err(InvalidOperationError::BookNotFound),
+                    }
+                    let currency_total = totals.entry(entry.currency).or_insert(0);
+                    *currency_total += entry_amount;
+                }
+                None => todo!(),
             }
         }
         if totals.iter().any(|(_, total)| *total != 0) {
-            Err(InvalidOperationError)
+            Err(InvalidOperationError::InvariantViolation)
         } else {
-            Ok(Self(op))
+            Ok(Self {
+                idempotency_key: op.idempotency_key,
+                entries: preprocessed_entries,
+            })
         }
     }
 }
 
 #[derive(Debug, Error)]
-pub struct InvalidOperationError;
+pub struct WriteMismatchError;
+
+impl From<std::io::Error> for WriteMismatchError {
+    fn from(e: std::io::Error) -> Self {
+        println!("Error while compacting {}", e);
+        WriteMismatchError
+    }
+}
+
+impl Display for WriteMismatchError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        //TODO better error info
+        f.write_str("Data size didn't match target serialization format")
+    }
+}
+#[derive(Debug, Error)]
+pub enum InvalidOperationError {
+    InvariantViolation,
+    BookNotFound,
+}
 
 impl Display for InvalidOperationError {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         //TODO better error info
         f.write_str("Invalid operation structure")
+    }
+}
+
+impl From<TryFromSliceError> for InvalidOperationError {
+    fn from(_: TryFromSliceError) -> Self {
+        InvalidOperationError::InvariantViolation
     }
 }
 
@@ -104,27 +269,55 @@ struct Acceptor {
 
 impl Acceptor {
     async fn handle(&mut self, cmd: AcceptorCommand) {
-        let entries = cmd.operations.0.entries;
+        let AcceptorCommand {
+            operations,
+            response_tx,
+        } = cmd;
+        let entries = &operations.entries;
+        let mut processed_entries = Vec::with_capacity(entries.capacity());
         let totals = entries.iter().fold(HashMap::new(), |mut t, entry| {
-            let running_total = t.entry(entry.account).or_insert(0i128);
-            *running_total += match entry.op_type {
-                //TODO consider account/book's accounting type
-                AccountType::DEBIT => -entry.amount,
-                AccountType::CREDIT => entry.amount,
-            };
+            // TODO: identify book_id, target_page, target_lin
+            processed_entries.push(ProcessedEntry {
+                target_book_id: 0u64,
+                amount: entry.amount,
+                ledger_code: [0; 8],
+                target_page: 0u32,
+                target_line: 0u16,
+            });
+            let running_total = t.entry(entry.target_account_id).or_insert((
+                entry.currency,
+                entry.balance_type,
+                0i128,
+            ));
+            running_total.2 += entry.amount;
             t
         });
 
-        //TODO re-validate balances
-        //TODO send OperationResult::Rejected
-        //TODO construct content string
+        //TODO re-validate balances against totals then send OperationResult::Rejected
 
-        let content = "".to_string();
-        self.buffer.push(WriteCommand {
-            content,
-            ending_balances: totals, //TODO replace with computed ending balance
-            response_tx: cmd.response_tx,
-        });
+        // TODO: identify checksum, prev_hash, operation_id
+        let processed = ProcessedOperation {
+            idempotency_key: operations.idempotency_key,
+            entries: processed_entries,
+            operation_id: 0u64,
+            checksum: 0u32,
+            prev_hash: [0; 32],
+        };
+
+        let compaction_result = processed.compacted();
+        match compaction_result {
+            Ok(content) => {
+                self.buffer.push(WriteCommand {
+                    content,
+                    ending_balances: totals, //TODO replace with computed ending balance
+                    response_tx,
+                });
+            }
+            Err(_) => {
+                println!("WriteMismatch");
+                let _ = response_tx.send(Err(OperationError::WriterSystemFailure));
+            }
+        }
     }
 
     async fn flush(&mut self) {
@@ -150,38 +343,38 @@ impl Acceptor {
         tokio::pin!(flush_timer);
         loop {
             tokio::select! {
-                    biased; // always prioritize trying to "receive" a message
-                    Some(cmd) = self.rx.recv() => {
-                        let buffer_was_empty = self.buffer.is_empty();
+                biased; // always prioritize trying to "receive" a message
+                Some(cmd) = self.rx.recv() => {
+                    let buffer_was_empty = self.buffer.is_empty();
 
-                        // TODO assess handle returning boolean vs self.buffer.is_empty()
-                        self.handle(cmd).await;
+                    // TODO assess handle returning boolean vs self.buffer.is_empty()
+                    self.handle(cmd).await;
 
-                        if buffer_was_empty && !self.buffer.is_empty() {
-                            flush_timer.as_mut().reset(Instant::now() + manual_flush_after);
-                        }
-
-                        if self.buffer.len() == self.buffer.capacity() {
-                            //TODO use standard logging
-                            println!("Full-flushing {}", self.buffer.len());
-                           self.flush().await;
-                        }
+                    if buffer_was_empty && !self.buffer.is_empty() {
+                        flush_timer.as_mut().reset(Instant::now() + manual_flush_after);
                     }
 
-                    _  = &mut flush_timer, if !self.buffer.is_empty() => {
+                    if self.buffer.len() == self.buffer.capacity() {
                         //TODO use standard logging
-                         println!("Timed flushing {}", self.buffer.len());
-                         self.flush().await;
-                    }
-
-                    //TODO Add other shutdown signals?
-                    _ = signal::ctrl_c()  => {
-                        if !self.buffer.is_empty() {
-                            self.flush().await;
-                        }
-                        break;
+                        println!("Full-flushing {}", self.buffer.len());
+                       self.flush().await;
                     }
                 }
+
+                _  = &mut flush_timer, if !self.buffer.is_empty() => {
+                    //TODO use standard logging
+                     println!("Timed flushing {}", self.buffer.len());
+                     self.flush().await;
+                }
+
+                //TODO Add other shutdown signals?
+                _ = signal::ctrl_c()  => {
+                    if !self.buffer.is_empty() {
+                        self.flush().await;
+                    }
+                    break;
+                }
+            }
         }
     }
 }
@@ -241,7 +434,7 @@ impl AcceptorHandle {
 #[derive(Debug)]
 pub enum OperationResult {
     /// Contains the resulting balances after the accepted operation
-    Accepted(HashMap<AccountId, i128>),
+    Accepted(HashMap<AccountId, (Currency, BalanceType, i128)>),
 
     /// Contains the AccountId whose balance caused the rejection
     Rejected(AccountId),
@@ -249,8 +442,8 @@ pub enum OperationResult {
 
 #[derive(Debug)]
 struct WriteCommand {
-    content: String,
-    ending_balances: HashMap<AccountId, i128>,
+    content: Vec<u8>,
+    ending_balances: HashMap<AccountId, (Currency, BalanceType, i128)>,
     response_tx: oneshot::Sender<Result<OperationResult, OperationError>>,
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #[macro_use]
 extern crate rocket;
 
+use std::collections::HashMap;
 use std::time::Duration;
 
 use crate::ApiError::{SystemError, Unprocessable};
@@ -9,11 +10,13 @@ use crate::acceptor_task::{AcceptorHandle, InvalidOperationError, ValidOperation
 use rocket::State;
 use rocket::serde::json::Json;
 use serde::{Deserialize, Serialize};
+use uuid::Uuid;
 
 mod acceptor_task;
-type AccountId = u32;
-type Currency = String;
-#[derive(Deserialize, Serialize, Copy, Clone, Debug)]
+type AccountId = u64;
+type BookId = u64;
+type Currency = u16;
+#[derive(Deserialize, Serialize, Copy, Clone, Debug, Eq, PartialEq)]
 enum AccountType {
     DEBIT,
     CREDIT,
@@ -28,6 +31,7 @@ pub enum BalanceType {
 
 #[derive(Deserialize, Debug)]
 struct Operation {
+    idempotency_key: Uuid,
     entries: Vec<OperationEntry>,
 }
 
@@ -38,6 +42,7 @@ struct OperationEntry {
     op_type: AccountType,
     amount: i128,
     currency: Currency,
+    ledger_code: String,
 }
 
 #[derive(Serialize)]
@@ -47,7 +52,8 @@ struct OperationResult {
 
 #[derive(Serialize)]
 struct BalanceDescriptor {
-    account: u32,
+    account: u64,
+    currency: Currency,
     balance_type: BalanceType,
     balance: i128,
 }
@@ -71,12 +77,43 @@ impl From<InvalidOperationError> for ApiError {
         ApiError::BadRequest("request validation failed".to_string())
     }
 }
+
+struct Account {
+    account_type: AccountType,
+    books: HashMap<(Currency, BalanceType), BookId>,
+}
+
 #[post("/operations", format = "application/json", data = "<operation>")]
 async fn post_operations(
     operation: Json<Operation>,
     acceptor_handle: &State<AcceptorHandle>, //This shouldn't be
 ) -> Result<Json<OperationResult>, ApiError> {
-    let valid_op = ValidOperation::parse(operation.0)?;
+    //TODO pre-load accounts
+    let loaded_accounts = HashMap::from([
+        (
+            1,
+            Account {
+                account_type: AccountType::CREDIT,
+                books: HashMap::from([
+                    ((123, BalanceType::Available), 1),
+                    ((123, BalanceType::Current), 2),
+                    ((123, BalanceType::Hold), 3),
+                ]),
+            },
+        ),
+        (
+            2,
+            Account {
+                account_type: AccountType::CREDIT,
+                books: HashMap::from([
+                    ((123, BalanceType::Available), 1),
+                    ((123, BalanceType::Current), 2),
+                    ((123, BalanceType::Hold), 3),
+                ]),
+            },
+        ),
+    ]);
+    let valid_op = ValidOperation::parse(operation.0, loaded_accounts)?;
     match acceptor_handle.submit(valid_op).await {
         Ok(Accepted(balances)) => Ok(Json(OperationResult {
             //TODO refactor this to a better mapping
@@ -84,8 +121,9 @@ async fn post_operations(
                 .into_iter()
                 .map(|x| BalanceDescriptor {
                     account: x.0,
-                    balance_type: BalanceType::Current,
-                    balance: x.1,
+                    currency: x.1.0,
+                    balance_type: x.1.1,
+                    balance: x.1.2,
                 })
                 .collect(),
         })),
@@ -99,7 +137,7 @@ async fn post_operations(
 
 #[rocket::main]
 async fn main() {
-    let acceptor_handle = acceptor_task::spawn(5, 5, Duration::from_millis(10));
+    let acceptor_handle = acceptor_task::spawn(10, 5, Duration::from_millis(10));
     let _ = rocket::build()
         .manage(acceptor_handle)
         .mount("/", routes![post_operations])
